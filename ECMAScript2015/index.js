47cb814678b577cffef770f6e692e79eb681cb35
durgarao <asapusirisha@gmail.com>
2020-06-15 15:40:44 +0530

    map method

--- ECMAScript2015/index.js
+++ ECMAScript2015/index.js
@@ -9,3 +9,10 @@ let fullnames=(a,b)=>{
     return "sum of given numbers:"+(a+b);
 }
 console.info(fullnames(2,3));
+
+//Array methods
+//map
+var arr1=["shivangi","sirisha","krystel"];
+arr1.map(function(i){
+    console.log(i);
+})
