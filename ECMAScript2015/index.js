2e962858dae57e3a790277c41b164c17b34e4a8f
durgarao <asapusirisha@gmail.com>
2020-06-15 15:24:17 +0530

    Arrow function

--- ECMAScript2015/index.js
+++ ECMAScript2015/index.js
@@ -2,4 +2,10 @@
 let names=function(firstName,lastName){
         return "fullName:"+firstName+" "+lastName;
 }
-document.writeln(names("sirisha","asapu"));
+console.log(names("sirisha","asapu"));
+
+// arrow function
+let fullnames=(a,b)=>{
+    return "sum of given numbers:"+(a+b);
+}
+console.info(fullnames(2,3));
