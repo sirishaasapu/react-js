7565aaee6b7d607616f55b2fbd4fbdc8604a2fc0
durgarao <asapusirisha@gmail.com>
2020-06-15 15:43:32 +0530

    map using arrow function

--- ECMAScript2015/index.js
+++ ECMAScript2015/index.js
@@ -16,3 +16,13 @@ var arr1=["shivangi","sirisha","krystel"];
 arr1.map(function(i){
     console.log(i);
 })
+
+//map using arrow function
+var arr1=["mohsin","shivangi","sirisha","krystel"];
+arr1.map(function(i){
+    console.log(i);
+   
+})
+arr1.map(function(i,index){
+    console.log(index);
+})
