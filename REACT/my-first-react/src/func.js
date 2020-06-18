b6d02400cb36554b8cd972fdac790c0cc43e5064
durgarao <asapusirisha@gmail.com>
2020-06-18 19:12:27 +0530

    Hook -useState:manipulating data onClick

--- REACT/my-first-react/src/func.js
+++ REACT/my-first-react/src/func.js
@@ -5,11 +5,21 @@ function Func(){
     //Hook:useState -assign value to function component
     const [courses]=useState(['b.tech','M.tech','D.Ed','B.ed']);
 
-    const [branch]=useState({"branch1":'cse',
+    const [branch,setData]=useState({
+                             "branch1":'cse',
                               "branch2":'ece',
                               "branch3":'eee',
                               "branch4":'civil',
-                              "branch5":'mech',})
+                              "branch5":'mech'});
+          //manipulating data using Hook                    
+        let changebranch=()=>
+             setData({
+            "branch1":'HTML5',
+            "branch2":'CSS3',
+            "branch3":'bootstrap',
+            "branch4":'Angular',
+            "branch5":'React'});
+   
     return (
         <div className='container mt-5'>
            <h4>This is external functional component</h4>
@@ -19,16 +29,24 @@ function Func(){
             <li>{courses[1]}</li>
            <li>{courses[2]}</li> 
            </ul>
-            <h5>Branches</h5>
+            <h5>Courses</h5>
             <ul>
            <li>{branch.branch1}</li>
            <li>{branch.branch2}</li>
            <li>{branch.branch3}</li>
            <li>{branch.branch4}</li>
            <li>{branch.branch5}</li>
-          
-          
            </ul>
+           {/* //calling */}
+           <button className="btn btn-primary mb-5" onClick={changebranch}>Change</button>
+             {/* (or)*/}
+           {/* <button className="btn btn-primary mb-5" onClick={()=>{
+            setData({
+           "branch1":'HTML5',
+            "branch2":'CSS3',
+            "branch3":'bootstrap',
+            "branch4":'Angular',
+            "branch5":'React'}); }}>Change</button> */}
         </div>
     );
 }
