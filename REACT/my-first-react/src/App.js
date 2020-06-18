7ce4b8b6663756add3b5d1effa5bab9d633e9685
durgarao <asapusirisha@gmail.com>
2020-06-18 16:45:58 +0530

    functional component  created

--- REACT/my-first-react/src/App.js
+++ REACT/my-first-react/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Func from './func'
 
 function App() {
   return (
     <div>
-      <h2 style={{color:'white',background:'#042e15',padding:'15px'}}>My first React Application</h2>
+      <div  style={{color:'white',background:'#042e15',padding:'15px'}}>
+      <h2 className="container">My first React Application</h2>
+      </div>
+      <Func/>
     </div>
   );
 }
---
+++ REACT/my-first-react/src/func.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Func(){
+    return (
+        <div className='container mt-5'>
+           <h4>This is external functional component</h4>
+        </div>
+    );
+}
+
+export default Func;
