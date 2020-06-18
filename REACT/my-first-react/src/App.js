338ff373a337dd8c9c1245caab1e92c264d9e47e
durgarao <asapusirisha@gmail.com>
2020-06-18 16:14:16 +0530

    Basic initial html tags and styling in react added

---
+++ REACT/my-first-react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2 style={{color:'white',background:'#042e15',padding:'15px'}}>My first React Application</h2>
+    </div>
+  );
+}
+
+export default App;
