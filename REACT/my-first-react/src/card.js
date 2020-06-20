e52f094346f362d1b3a515938943d34132ad997b
durgarao <asapusirisha@gmail.com>
2020-06-20 15:06:59 +0530

    card-resume with navigation #task

--- REACT/my-first-react/src/card.js
+++ REACT/my-first-react/src/card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from './data/carded.json'
+import data from './data/carded.json'
 // import Profilei from './images/woman.svg';
 import {Link}  from 'react-router-dom';
 
@@ -7,18 +7,18 @@ function card(){
    return(
        <div className="container mt-5">
            <div className="row justify-content-center">
-           {Card.profiles.map((value,index)=>(
+           {data.profiles.map((value,index)=>(
            <div className="col-3 card m-3" style={{boxShadow:'1px 1px 1px 1px'}}>
            <div className="card-body text-center">
-           <img src={value.images} alt="profilepic" style={{width:'70%'}}/>
+           <img src={value.basics.images} alt="profilepic" style={{width:'70%'}}/>
             {/* <img src={require('./Images/Woman.svg')}/> */}
-            <h4 className="mt-2">{value.name}</h4>
-            <h5 style={{color:'grey'}}>{value.Id}</h5>
-            <h5>{value.role}</h5>
+            <h4 className="mt-2">{value.basics.name}</h4>
+            <h5 style={{color:'grey'}}>{value.basics.Id}</h5>
+            <h5>{value.basics.role}</h5>
             <hr/>
-            <h5 style={{color:'purple'}}>{value.Email}</h5>
-            <h5 style={{color:'purple'}}>{value.contact}</h5>
-            <Link to={{pathname:'/resume'}} className="btn btn-primary mt-3">view profile</Link>
+            <h5 style={{color:'purple'}}>{value.basics.Email}</h5>
+            <h5 style={{color:'purple'}}>{value.basics.contact}</h5>
+            <Link to={{pathname:'/resume',data:{id:index}}} type="button" className="btn btn-primary mt-3 w-auto">view profile</Link>
            </div>
            </div>
            ))}
